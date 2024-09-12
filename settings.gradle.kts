6b797583b22ab6642d7816b9baa4524dab28593d
fadybassem <f.bassem@icommunity-app.com>
2024-09-12 19:00:42 +0300

    init project

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Movie Discovery App"
+include(":app")
+include(":presentation")
+include(":domain")
+include(":data")
+include(":di")
+include(":util")
