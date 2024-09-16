9c6c4677c45cea4194229dfb2272ed3f808394af
fadybassem <f.bassem@icommunity-app.com>
2024-09-17 01:52:29 +0300

    upload apk

--- domain/src/main/java/com/fadybassem/domain/usecase/details/GetMovieDetailsCreditsUseCase.kt
+++ domain/src/main/java/com/fadybassem/domain/usecase/details/GetMovieDetailsCreditsUseCase.kt
@@ -10,7 +10,16 @@ import javax.inject.Inject
 class GetMovieDetailsCreditsUseCase @Inject constructor(private val repository: MovieRepository) {
     fun execute(id: Int): Flow<Resource<Credits>> = flow {
         repository.getMovieDetailsCredits(id = id).collect { resource ->
-            emit(resource)
+            if (resource is Resource.Success) {
+                val credits = resource.data
+                credits?.let {
+                    it.cast = ArrayList(it.cast.distinctBy { castMember -> castMember.id })
+                    it.crew = ArrayList(it.crew.distinctBy { crewMember -> crewMember.id })
+                }
+                emit(Resource.Success(credits))
+            } else {
+                emit(resource)
+            }
         }
     }
 }
